ba0b03a2760714ff715c49982f29973768c4ae90
sumedh3113 <sumedhdeshpande31@gmail.com>
2018-07-18 18:54:00 -0700

    update

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -2,3 +2,22 @@
  const ganache = require('ganache-cli');
  const Web3 = require('Web3');
  const web3 =new Web3(ganache.provider());
+ const {interface , bytecode} = require('../compile');
+
+let accounts;
+let inbox;
+
+beforeEach( async ()=>{
+  accounts = await web3.eth.getAccounts();
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+  .deploy({data :bytecode, arguments :['Hi there'] })
+  .send({from: accounts[0], gas: '1000000'})
+ });
+
+describe('Inbox', ()=>{
+                  it('deployss a contract', ()=>{
+                      console.log(inbox)
+                  });
+
+});
