fa62a097071244e482e74701ee6251ac01fcb4bc
sumedh3113 <sumedhdeshpande31@gmail.com>
2018-07-20 02:28:06 -0700

    update

---
+++ deploy.js
@@ -0,0 +1,28 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const {interface , bytecode} = require('./compile');// here we use only one dot as in same directory with compile.js
+
+const provider = new HDWalletProvider(
+'blade develop disease hospital first kangaroo brown march market record corn engine',
+'https://rinkeby.infura.io/v3/19f06dc35ffd41b7a57477f27a85f06f'
+
+
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async ()=>{
+
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({data: '0x'+ bytecode, arguments: ['Hi there']})
+    .send({from: accounts[0]}); // remove 'gas'
+
+
+ console.log('Contract deployed to ', result.options.address);
+};
+
+deploy();
--- test/inbox.test.js
+++ test/inbox.test.js
@@ -1,7 +1,12 @@
  const assert = require('assert');
  const ganache = require('ganache-cli');
  const Web3 = require('Web3');
- const web3 =new Web3(ganache.provider());
+ //const web3 =new Web3(ganache.provider());
+
+ //New changes
+ const provider = ganache.provider();
+ const web3 = new Web3(provider);
+
  const {interface , bytecode} = require('../compile');
 
 let accounts;
@@ -9,15 +14,29 @@ let inbox;
 
 beforeEach( async ()=>{
   accounts = await web3.eth.getAccounts();
-
+// here we are deploying our code to ganache network
   inbox = await new web3.eth.Contract(JSON.parse(interface))
   .deploy({data :bytecode, arguments :['Hi there'] })
   .send({from: accounts[0], gas: '1000000'})
+
+  // ADD THIS ONE LINE RIGHT HERE!!!!! <---------------------
+  inbox.setProvider(provider);
  });
 
 describe('Inbox', ()=>{
+  //to check address assigned
                   it('deployss a contract', ()=>{
-                      console.log(inbox)
+                  assert.ok(inbox.options.address);
                   });
-
+//to verify message field
+                it('message', async () =>{
+                    const message = await inbox.methods.message().call();
+                    assert.equal(message,'Hi there!');
+                });
+// to verify setMessage function
+              it('change message', async () =>{
+                inbox.methods.setMessage('bye').send({from:accounts[0]});
+                  const message = await inbox.methods.message().call();
+                  assert.equal(message,'Hi there!');
+                });
 });
